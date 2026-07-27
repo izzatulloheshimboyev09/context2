b1705716d65544ac49bd7f96028f94c2d1226504
izzatulloheshimboyev09 <izzatulloheshimboyev09@gmail.com>
2026-07-27 21:55:40 +0500

    input.jsx full fixed

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 
-function Input() {
-  return <div></div>;
+function Input({ search, setSearch }) {
+  return (
+    <div className="relative my-6 max-w-md mx-auto">
+      <input
+        type="text"
+        placeholder="Search..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="w-full pl-4 pr-10 py-2 border-2 rounded-xl outline-none focus:border-blue-700 transition"
+      />
+      <FaSearch className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 pointer-events-none" />
+    </div>
+  );
 }
 
 export default Input;
--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -4,30 +4,42 @@ import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import { imagesData } from "../data/gallery";
 
 import Navbar from "../components/Navbar";
+import Input from "../components/Input";
 
 function GalleryPage() {
   const [sliceCount, setSliceCount] = useState(10);
+  const [search, setSearch] = useState("");
 
+  const filteredImages = imagesData.filter((img) =>
+    img.title.toLowerCase().includes(search.toLowerCase()),
+  );
   return (
     <main>
       <Navbar />
+      <Input search={search} setSearch={setSearch} />
       <div className="align-center">
         <ResponsiveMasonry>
           <Masonry>
-            {imagesData &&
-              imagesData.slice(0, sliceCount).map((img, index) => {
+            {filteredImages &&
+              filteredImages.slice(0, sliceCount).map((img, index) => {
                 return (
                   <Link
                     key={index}
                     to={`/${img.id}`}
                     className="block transition-all duration-300 hover:-translate-y-1 hover:shadow-lg rounded-lg overflow-hidden"
                   >
-                    <img src={img.src} alt="" />
+                    <img src={img.src} alt={img.title} />
                   </Link>
                 );
               })}
           </Masonry>
         </ResponsiveMasonry>
+
+        {filteredImages.length === 0 && (
+          <p className="text-center text-gray-500 my-8">
+            Hech qanday rasm topilmadi.
+          </p>
+        )}
 
         <button
           onClick={() => {
