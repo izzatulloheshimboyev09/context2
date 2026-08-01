383962eb71dbe9ded10796a20d5f7df6ba105d46
izzatulloheshimboyev09 <izzatulloheshimboyev09@gmail.com>
2026-08-01 15:42:54 +0500

    app.js changed

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 
 import { RouterProvider } from "react-router-dom";
-import { routes } from "./App.jsx";
+import App from "./App.jsx";
 import { GalleryContext } from "./context/GlobalContext.jsx";
 import { GlobalContextProvider } from "./context/GlobalContext.jsx";
 createRoot(document.getElementById("root")).render(
   <GlobalContextProvider>
-    <RouterProvider router={routes} />
+    <App />
   </GlobalContextProvider>,
 );
